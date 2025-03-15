54731da6ed22fa0602af64e701944399a302a0f3
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-03-15 22:34:45 +0600

    name and exclamation mark problem solved

---
+++ part01/08_name_and_address.py
@@ -0,0 +1,8 @@
+first_name = input("Given name: ")
+last_name = input("Family name: ")
+street_address = input("Street address: ")
+city_postal_code = input("City and postal code: ")
+
+print(first_name + " " + last_name)
+print(street_address)
+print(city_postal_code)
