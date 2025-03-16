23ff29f5067d85a1b936ef46deb620154989d6d1
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-03-16 12:15:12 +0600

    product problem solved

---
+++ part01/16_product.py
@@ -0,0 +1,7 @@
+n1 = int(input("Please type in the first number: "))
+n2 = int(input("Please type in the second number: "))
+n3 = int(input("Please type in the third number: "))
+
+product = n1 * n2 * n3
+
+print("The product is", product)
