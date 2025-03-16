33f139a6257cdc5efdf931400f1154e9084c4c41
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-03-16 12:10:02 +0600

    name and age problem solved

---
+++ part01/14_name_and_age.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+year = int(input("Which year were you born? "))
+age = 2021 - year
+print("Hi " + name + ", you will be " + str(age) +
+      " years old at the end of the year 2021")
