e6ba236a94a33fe91858ed93c9b79721a183c805
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-03-16 12:19:05 +0600

    sum and product problem solved

---
+++ part01/17_sum_and_product.py
@@ -0,0 +1,8 @@
+number1 = int(input("Number 1: "))
+number2 = int(input("Number 2: "))
+
+sum = number1 + number2
+product = number1 * number2
+
+print("The sum of the numbers:", sum)
+print("The product of the numbers:", product)
