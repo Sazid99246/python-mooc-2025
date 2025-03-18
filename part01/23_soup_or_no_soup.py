911bd5a365f0f1398f268e9abd8cf5fe55b34b23
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-03-18 08:08:49 +0600

    Soup or no soup problem solved

---
+++ part01/23_soup_or_no_soup.py
@@ -0,0 +1,6 @@
+name = input("Please tell me your name: ")
+if name != "Jerry":
+    soup_portions = int(input("How many portions of soup? "))
+    total_cost = soup_portions * 5.90
+    print("The total cost is", total_cost)
+print("Next please!")
