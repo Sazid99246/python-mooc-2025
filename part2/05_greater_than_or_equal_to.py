5453bb0cc0277206b3343a005708ec0638fd280a
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-03-20 09:04:37 +0600

    greater than or equal to problem solved

---
+++ part2/05_greater_than_or_equal_to.py
@@ -0,0 +1,9 @@
+n1 = int(input("Please type in the first number: "))
+n2 = int(input("Please type in the second number: "))
+
+if n1 > n2:
+    print("The greater number was", n1)
+elif n2 > n1:
+    print("The greater number was", n2)
+else:
+    print("The numbers are equal!")
