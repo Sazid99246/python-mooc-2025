1ea9755a48f3230398c01264296b31c5e9f5dc69
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-03-29 09:11:53 +0600

    some code deleted

--- part04/04_square_of_hashes.py
+++ part04/04_square_of_hashes.py
@@ -13,19 +13,3 @@ def square_of_hashes(size):
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     square_of_hashes(5)
-# Copy here code of line function from previous exercise
-# Copy here code of line function from previous exercise
-def line(n, text):
-    if len(text) == 0:
-        print(n * "*")
-    else:
-        print(text[0] * n)
-
-def square_of_hashes(size):
-    # You should call function line here with proper parameters
-    for i in range(size):
-        line(size, "#")
-
-# You can test your function by calling it within the following block
-if __name__ == "__main__":
-    square_of_hashes(5)
