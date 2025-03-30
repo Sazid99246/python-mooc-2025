8def0f7034444f7a236ebf1b7af177538727358c
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-03-30 10:00:52 +0600

    sum of positives problem solved

---
+++ part04/25_sum_of_positives.py
@@ -0,0 +1,11 @@
+def sum_of_positives(nums):
+    sum = 0
+    for num in nums:
+        if num > 0:
+            sum += num
+    return sum
+
+if __name__ == "__main__":
+    my_list = [1, -2, 3, -4, 5]
+    result = sum_of_positives(my_list)
+    print("The result is", result)
