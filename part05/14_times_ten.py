2aa4319ef809514f15f86ce856c4bd00b129d3e8
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-04-02 05:03:19 +0600

    times ten problem solved

---
+++ part05/14_times_ten.py
@@ -0,0 +1,12 @@
+def times_ten(start_index: int, end_index: int):
+    result = {}
+
+    for i in range(start_index, end_index + 1):
+        result[i] = i * 10
+
+    return result
+
+
+if __name__ == "__main__":
+    d = times_ten(3, 6)
+    print(d)
