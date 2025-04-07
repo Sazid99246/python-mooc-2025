a39e8f96df81908537829e75f130b185b2df6d3d
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-04-07 10:03:45 +0600

    fractions problem solved

---
+++ part07/03_fractions.py
@@ -0,0 +1,8 @@
+from fractions import Fraction
+
+
+def fractionate(amount: int):
+    fractions = []
+    for i in range(amount):
+        fractions.append(Fraction(1, amount))
+    return fractions
